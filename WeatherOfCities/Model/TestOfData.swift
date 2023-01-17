133ef1148359eb4986befe85900870fd557792ed
Askerov Adilet <askerov888@gmail.com>
2023-01-17 08:41:46 +0600

    first commit

---
+++ WeatherOfCities/Model/TestOfData.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+typealias Coordinates = (Double, Double)
+
+class TestOfData {
+	var sitiesAndCoordinates: [String: Coordinates] = [
+		"Bishkek": (1, 1),
+		"London": (1, 1),
+		"New York": (1, 1),
+		"Los Angeles": (1, 1),
+		"Paris": (1, 1),
+		"Tokyo": (1, 1),
+		"Sydney": (1, 1),
+		"Rio De Janeiro": (1, 1),
+		"Hong Kong": (1, 1),
+		"Dubai": (1, 1)
+	]
+}
