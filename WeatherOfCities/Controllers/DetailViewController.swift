ed0afbfe3740442d6f5e7d68dafdc6c6802f52df
Askerov Adilet <askerov888@gmail.com>
2023-01-18 09:01:59 +0600

    prepare view in detailVC

--- WeatherOfCities/Controllers/DetailViewController.swift
+++ WeatherOfCities/Controllers/DetailViewController.swift
@@ -5,7 +5,15 @@ class DetailViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
     }
+	
+	@IBOutlet weak var image: UIImageView!
+	@IBOutlet weak var nameOfSity: UILabel!
+	@IBOutlet weak var temp: UILabel!
+	@IBOutlet weak var day1: UILabel!
+	@IBOutlet weak var day2: UILabel!
+	@IBOutlet weak var day3: UILabel!
+	@IBOutlet weak var day4: UILabel!
+	@IBOutlet weak var day5: UILabel!
 
 }
