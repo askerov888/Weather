1bfb63651db7907995dd88723574683f38e038b6
Askerov Adilet <askerov888@gmail.com>
2023-01-18 08:32:41 +0600

    finish work listVC

--- WeatherOfCities/Controllers/ListViewController.swift
+++ WeatherOfCities/Controllers/ListViewController.swift
@@ -1,28 +1,31 @@
 import UIKit
 
-class ListViewController: UIViewController, UITableViewDataSource {
-	let dataSource = TestOfData()
+class ListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+	}
+
+	let dataSource = TestOfData().sities.sorted(by: <)
 	
 	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-		dataSource.sities.count
+		dataSource.count
 	}
 	
 	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-		let cell = UITableViewCell(style: .default, reuseIdentifier: "Cell")
+		let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
 		
 		var content = cell.defaultContentConfiguration()
-		content.text = dataSource.sities[indexPath.row]
+		content.text = dataSource[indexPath.row]
+		content.textProperties.font = UIFont.systemFont(ofSize: 20)
+		content.textProperties.alignment = .center
 		cell.contentConfiguration = content
 		
+		cell.layer.borderColor = UIColor.systemTeal.cgColor
+		cell.layer.borderWidth = CGFloat(5)
+		cell.layer.cornerRadius = 15
 		return cell
 	}
 	
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-	
 	@IBOutlet weak var tableView: UITableView!
-    
-	
 }
---
+++ WeatherOfCities/View/SityCell.swift
@@ -0,0 +1,23 @@
+//
+//  SityCell.swift
+//  WeatherOfCities
+//
+//  Created by jarvis on 18/1/23.
+//
+
+import UIKit
+
+class SityCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
