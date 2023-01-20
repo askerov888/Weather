8277400c4b5dd2e4ff17d00422b30bccc845a1c1
Askerov Adilet <askerov888@gmail.com>
2023-01-20 12:31:42 +0600

    changed listVC, added viewImage

--- WeatherOfCities/Controllers/ListViewController.swift
+++ WeatherOfCities/Controllers/ListViewController.swift
@@ -4,5 +4,12 @@ class ListViewController: UIViewController {
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
+		viewImage.image = UIImage(named: "clods2")
+		textField.layer.borderColor = UIColor.gray.cgColor
+		textField.layer.borderWidth = 3
+		textField.layer.cornerRadius = 10
 	}
+	@IBOutlet var viewImage: UIImageView!
+	@IBOutlet var searchButton: UIButton!
+	@IBOutlet var textField: UITextField!
 }
