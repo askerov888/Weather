2aff0fbadb4a70650f6d5017ffbae5d1f4bb6a2d
Askerov Adilet <askerov888@gmail.com>
2023-01-22 11:06:25 +0600

    Added image, max & min temp. Changed font, constraint of name and temp

--- WeatherOfCities/Controllers/DetailViewController.swift
+++ WeatherOfCities/Controllers/DetailViewController.swift
@@ -4,18 +4,20 @@ class DetailViewController: UIViewController {
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
-//		let _ = {
-//			let editScreen = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "ListViewController") as! ListViewController
-//		}()
 		taskGo()
 	}
 	
 	var sity = ""
 	
 	func taskGo() {
-		let url = URL(string:"https://api.openweathermap.org/data/2.5/weather?appid=\(API.key)&units=metric&q=\(sity)")!
+		let session = URLSession.shared
 		
-		let task = URLSession.shared.dataTask(with: url) { data, response, error in
+		let sityWithoutSpace = sity.replacingOccurrences(of: " ", with: "%20")
+		
+		let url = URL(string:"https://api.openweathermap.org/data/2.5/weather?appid=\(API.key)&units=metric&q=\(sityWithoutSpace)")!
+		
+		
+		let taskURL = session.dataTask(with: url) { data, response, error in
 			if error != nil {
 				print("Error")
 			}
@@ -30,21 +32,29 @@ class DetailViewController: UIViewController {
 				return
 			}
 			
+			guard let icon: String = result.weather?[0].icon else {return}
+			
+			let urlImage = URL(string: "https://openweathermap.org/img/wn/\(icon)@4x.png")!
+
+			guard let dataImage = try? Data(contentsOf: urlImage) else {
+				return
+			}
+			
 			DispatchQueue.main.async {
 				self.nameOfSity.text = result.name!
 				self.temp.text = String(format: "%0.f", result.main?.temp as! CVarArg)
+				self.minTemp.text = String(format: "%0.f", result.main?.temp_min as! CVarArg) + "\n min"
+				self.maxTemp.text = String(format: "%0.f", result.main?.temp_max as! CVarArg) + "\n max"
+				self.image.image = UIImage(data: dataImage)
 			}
 		}
-		task.resume()
+		taskURL.resume()
 	}
 	
 	@IBOutlet weak var image: UIImageView!
 	@IBOutlet weak var nameOfSity: UILabel!
 	@IBOutlet weak var temp: UILabel!
-	@IBOutlet weak var day1: UILabel!
-	@IBOutlet weak var day2: UILabel!
-	@IBOutlet weak var day3: UILabel!
-	@IBOutlet weak var day4: UILabel!
-	@IBOutlet weak var day5: UILabel!
+	@IBOutlet weak var minTemp: UILabel!
+	@IBOutlet weak var maxTemp: UILabel!
 
 }
